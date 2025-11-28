07acb59c2ba5e4d3aa09f8a37c4d0997d90098f1
sofia_a1 <alexandra.sofia@softcom-int.com>
2025-11-28 07:12:36 +0200

    update migrations

--- ui/models.py
+++ ui/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class UserQueryLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     query_text = models.TextField()
+    params = models.TextField(default=None)
     executed_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
